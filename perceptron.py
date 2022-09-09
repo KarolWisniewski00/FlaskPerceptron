c57a13c1cfee8a580a815a736d0af40722ce4bfc
Karol Wiśniewski <karol.wisniewski2901@gmail.com>
2022-09-09 15:16:52 +0100

    Add view usig, change functionality of perceptron

--- perceptron.py
+++ perceptron.py
@@ -1,14 +1,19 @@
 import random
 
 class Perceptron():
-    def __init__(self, dataTable):                          #dataTable - lista w liście(im więcej tym lepiej)
-        self.weightFunction1=random.randrange(-1000,1000)   #Losowa waga 1 do funkcji aktywacji
-        self.weightFunction2=random.randrange(-1000,1000)   #Losowa waga 2 do funkcji aktywacji    
-        self.weightBias=random.randrange(-1000,1000)        #Losowa waga bias do funkcji aktywacji
-        self.step=0.2                                       #Szybkość uczenia neuronu 
-        self.dataTable=dataTable                            #Dane treningowe [Długość, Średnica, Rodzaj] - Obrączka bądź długopis
+    def appendVariables(self,step=0.2):
+        #VARIABLES - ZMIENNE
+        self.weightFunction1=random.randrange(-1000,1000)   #WEIGHT FUNCTION ACTIVATION 1 - WAGA FUNKCJI AKTYWACJI 1
+        self.weightFunction2=random.randrange(-1000,1000)   #WEIGHT FUNCTION ACTIVATION 2 - WAGA FUNKCJI AKTYWACJI 2
+        self.weightBias=random.randrange(-1000,1000)        #WEIGHT FUNCTION ACTIVATION BIAS - WAGA FUNKCJI AKTYWACJI BIAS
+        self.step=step                                      #STEP - KROK
+
+    def appendDataTable(self,dataTable):
+        #VARIABLES - ZMIENNE
+        self.dataTable=dataTable    #TRAINIG DATA - DANE TREINIGOWE
 
     def genDataTable(self):
+        #GENERATOR
         for row in self.dataTable:
             yield row[2]
 
@@ -16,7 +21,7 @@ class Perceptron():
         #VARIABLES - ZMIENNE
         resultat=[]
 
-        #CREATING NEURON SECTION - TWORZENIE NEURONU
+        #CREATING NEURON - TWORZENIE NEURONU
         for row in self.dataTable:
             res=self.weightBias+(row[0]*self.weightFunction1)+(row[1]*self.weightFunction2)
             if res>0:
@@ -27,25 +32,20 @@ class Perceptron():
 
     def training(self):
         #VARIABLES - ZMIENNE
-        counter=0       #Licznik poprawnych outputów
-        counter2=0      #Licznik ilości treningów
-        
-        #PRINT SECTION - POKAŻ NEURON
-        print('Neuron before traning:')
-        for i in self.neuron():
-            print(i)
+        self.counter1=0      #COUNTER GOOD OUTPUT - LICZNIK DOBRYCH OUTPUTÓW
+        self.counter2=0      #COUNTER TRAINING - LICZNIK TRENINGÓW
 
-        #TRAINING SECTION - TRENOWANIE
-        while counter<len(self.dataTable):
+        #TRAINING - TRENOWANIE
+        while self.counter1<len(self.dataTable):
             gen=self.genDataTable()
             tabelaResulatat=self.neuron()
 
-            counter2+=1
-            counter=0
+            self.counter2+=1
+            self.counter1=0
             for row in tabelaResulatat:
                 correct=next(gen)
                 if row[2] == correct:
-                    counter+=1
+                    self.counter1+=1
                 else:
                     if row[2]==1:
                         self.weightFunction1=self.weightFunction1+(self.step*1*row[0])
@@ -55,36 +55,19 @@ class Perceptron():
                         self.weightFunction1=self.weightFunction1+(self.step*-1*row[1])
                         self.weightFunction2=self.weightFunction2+(self.step*-1*row[1])
                         self.weightBias=self.weightBias+(self.step*-1*row[1])
-        
-        #PRINT SECTION - POKAŻ NEURON
-        print('Numbers of training:',counter2)
-        print('Neuron after training:')
-        for i in self.neuron():
-            print(i)
+        return self.counter2
         
     def use(self, input1, input2):
-        #FUNCTION ACTIVATION SECTION
+        #FUNCTION ACTIVATION - FUNKCJA AKTYWACJI
         output=self.weightBias+(input1*self.weightFunction1)+(input2*self.weightFunction2)
 
-        #PRINT SECTION - POKAŻ OUTPUTA
+        #PRINT OUTPUT - POKAŻ OUTPUTA
         if output>0:
-            print('Output from neuron: 0')
+            return 0
         elif output<0:
-            print('Output from neuron: 1')
+            return 1
 
-
-if __name__=='__main__':
-    #EXAMPLE
-    perceptron=Perceptron([
-            [23,4,1],
-            [18,3,1],
-            [8,2,1],
-            [200,30,1],
-            [8,150,0],
-            [30,350,0],
-            [30,100,0],
-            [10,200,0],
-        ])
-    perceptron.training()
-    perceptron.use(200,35)
+    def getTrainingCounter(self):
+        #RETURN COUNTER - ZWRÓC LICZNIK
+        return self.counter2
     
